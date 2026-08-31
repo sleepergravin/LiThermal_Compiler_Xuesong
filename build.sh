2a596302c54ab5f3884770d4f05733a8b3da367d
sleepergravin <sleepergravin@users.noreply.github.com>
2026-08-31 14:30:37 +0800

    Update submodule: custom boot logo

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ if [ -d $ROOTPATH/LiThermal ]; then
     cd ..
 else
     echo "Folder not exist, cloning..."
-    git clone https://github.com/diylxy/LiThermal.git
+    git clone https://github.com/sleepergravin/LiThermal_Xuesong.git
 fi
 
 export STAGING_DIR=$ROOTPATH/target
